9a1eecd0dd73918e05c472814e6d5cd364767f31
UjwalaFalak <156996303+UjwalaFalak@users.noreply.github.com>
2024-01-27 09:29:25 +0530

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int source_node,Vertex,Edge,time,visited[10],Graph[10][10];
-
-void DepthFirstSearch(int i)
+/*          ADJACENCY MATRIX                            */
+int source,V,E,time,visited[20],G[20][20];
+void DFS(int i)
 {
     int j;
     visited[i]=1;
     printf(" %d->",i+1);
-    for(j=0;j<Vertex;j++)
+    for(j=0;j<V;j++)
     {
-        if(Graph[i][j]==1&&visited[j]==0)
-
-            DepthFirstSearch(j);
+        if(G[i][j]==1&&visited[j]==0)
+            DFS(j);
     }
 }
-
 int main()
 {
     int i,j,v1,v2;
-    printf("\t\t\tDepth_First_Search\n");
-    printf("Enter the number of edges:");
-    scanf("%d",&Edge);
-    printf("Enter the number of vertices:");
-    scanf("%d",&Vertex);
-    for(i=0;i<Vertex;i++)
+    printf("\t\t\tGraphs\n");
+    printf("Enter the no of edges:");
+    scanf("%d",&E);
+    printf("Enter the no of vertices:");
+    scanf("%d",&V);
+    for(i=0;i<V;i++)
     {
-        for(j=0;j<Vertex;j++)
-            Graph[i][j]=0;
+        for(j=0;j<V;j++)
+            G[i][j]=0;
     }
-    for(i=0;i<Edge;i++)
+    /*    creating edges :P    */
+    for(i=0;i<E;i++)
     {
-        printf("Enter the edges (V1 V2) : ");
+        printf("Enter the edges (format: V1 V2) : ");
         scanf("%d%d",&v1,&v2);
-        Graph[v1-1][v2-1]=1;
+        G[v1-1][v2-1]=1;
+
     }
-    for(i=0;i<Vertex;i++)
+
+    for(i=0;i<V;i++)
     {
-        for(j=0;j<Vertex;j++)
-            printf(" %d ",Graph[i][j]);
+        for(j=0;j<V;j++)
+            printf(" %d ",G[i][j]);
         printf("\n");
     }
     printf("Enter the source: ");
-    scanf("%d",&source_node);
-    DepthFirstSearch(source_node-1);
+    scanf("%d",&source);
+        DFS(source-1);
     return 0;
 }
