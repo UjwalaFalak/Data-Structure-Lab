64b8e1bb340e0efe610b26a2251a069a5160327c
UjwalaFalak <156996303+UjwalaFalak@users.noreply.github.com>
2024-01-27 09:22:33 +0530

    Update adlist.c

--- adlist.c
+++ adlist.c
@@ -5,11 +5,11 @@
 typedef struct node
 {
    int vertexnum;
-    Struct node *next;
+    struct node *next;
 }node;
 typedef struct list
 {
-    Node * head;
+    node * head;
 }list;
 list * adjlist[maxnode]={0};
 
@@ -29,22 +29,21 @@ printlist();
 
 void addnode(int s,int d)
 {
-	Node *src,*temp,*dest;
+	node *src,*temp,*dest;
 	if(adjlist[s]->head==NULL)
 	{
 		src=(node *)malloc(sizeof(node));
 		src->vertexnum=s;
 		src->next=NULL;
-		djlist[s]->head=src;
+		adjlist[s]->head=src;
 	}
 	dest=(node *)malloc(sizeof(node));
 	dest->vertexnum=d;
 	src->next=NULL;
-	temp=djlist[s]->head;
+	temp=adjlist[s]->head;
 	while(temp->next!=NULL)
-	temp->temp->next;
+	temp=temp->next;
 	temp->next=dest;
-    }
 }
  void printlist()
 {
@@ -52,11 +51,12 @@ int i;
 	for(i=1;i<maxnode;++i)
 	{
 		node *p=adjlist[i]->head;
-		printf(“Adjacency list of vertex%d”,i);
+		printf("Adjacency list of vertex%d",i);
 		while(p)
 		{
-			printf(“%d”,p->vertexnum);
+			printf("%d",p->vertexnum);
 			p=p->next;
 		}
-	printf(“\n”);
+	printf("\n");
+	}
 }
