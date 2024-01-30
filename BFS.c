93c27a7943a090c45b008ffc4ef9f2ff388c7c4c
UjwalaFalak <156996303+UjwalaFalak@users.noreply.github.com>
2024-01-30 10:21:02 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,42 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-int twodimarray[10][10],queue[10],visited[10],n,i,j,front=0,rear=-1;
-
-void breadthfirstsearch(int vertex) // breadth first search function
+int arr[10][10],queue[10],visited[10],n,i,j,front=0,rear=-1;
+void BFS(int ver)
 {
-      for (i=1;i<=n;i++)
-      if(twodimarray[vertex][i] && !visited[i])
-      queue[++rear]=i;
-      if(front<=rear)
-      {
-            visited[queue[front]]=1;
-            breadthfirstsearch(queue[front++]);
-      }
+  for(i=1;i<=n;i++)
+  {
+    if(arr[ver][i] && !visited[i])
+    	queue[++rear]=i;
+    if(front<=rear) 
+    {
+     	visited[queue[front]]=1;
+     	BFS(queue[front++]);
+    }
+  }
 }
-
 int main()
 {
-      int x;
-      printf("\n Enter the number of vertices:");
-      scanf("%d",&n);
-      for (i=1;i<=n;i++)
-      {
-            queue[i]=0;
-            visited[i]=0;
-      }
-      printf("\n Enter graph value in form of matrix:\n");
-      for (i=1;i<=n;i++)
-            for (j=1;j<=n;j++)
-                  scanf("%d",&twodimarray[i][j]);
-                  printf("\n Enter the source node:");
-                  scanf("%d",&x);
-                  breadthfirstsearch(x);
-                  printf("\n The nodes which are reachable are:\n");
-      for (i=1;i<=n;i++)
-      if(visited[i])
-            printf("%d\t",i);
-      else
-            printf("\n Breadth first search is not possible");
-      return 0;
+  int x;
+  printf("\nenter the number of vertices");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {  queue[i]=0;
+     visited[i]=0;
+  }
+   printf("\nenter the graph value in the form of matrix\n");
+  for(i=1;i<=n;i++)
+  	for(j=1;j<=n;j++)
+  		scanf("%d",&arr[i][j]);
+  printf("\nenter the source node\n");
+  scanf("%d",&x);
+  BFS(x);
+  printf("the nodes which are reachable are\n");
+  for(i=1;i<=n;i++)
+  {
+  	if(visited[i])
+  		printf("%d\t",i);
+  	else
+  		printf("\nBFS not possible");
+  }
+  return 0;
 }
